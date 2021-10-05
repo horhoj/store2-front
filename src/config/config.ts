35bc27da8f461e23f063a096189f9eca156c6d7f
horhoj <horhoj@yandex.ru>
2021-10-05 18:32:49 +0500

    set up environment variables

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,10 +1,11 @@
 export const DEFAULT_SIGN_IN_EMAIL = 'xman@mail.ru';
 export const DEFAULT_SIGN_IN_PASSWORD = 'p@ssw0rd';
 
-export const IS_LOGGING_ENABLED = true;
+export const IS_LOGGING_ENABLED =
+  process.env.REACT_APP_IS_LOGGING_ENABLED === 'TRUE';
 
-export const API_URL = 'http://192.168.0.55:8080/api/v1';
-// export const API_URL = 'http://localhost:8080/api/v1';
+export const API_URL =
+  process.env.REACT_APP_BASE_URL || 'http://localhost:8080/api/v1';
 
 export const DEFAULT_REQUEST_HEADERS = {
   Accept: 'application/json',
