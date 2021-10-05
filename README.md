9c5b0d4a9e50a41f0bd3d67cfdd6da538b2be7d2
horhoj <horhoj@yandex.ru>
2021-10-06 00:37:11 +0500

    add project description on home page and in README

--- README.md
+++ README.md
@@ -1,46 +1,26 @@
-# Getting Started with Create React App
+#Тестовый проект "Административная панель управления"
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+Это демонстрационный проект, написанный с целью показать уровень
+владения навыками разработки на react.
 
-## Available Scripts
+Посмотреть как работает можно здесь
 
-In the project directory, you can run:
+http://89.108.88.53:83
 
-### `yarn start`
+#Зависимости
+>docker docker-compose make linux
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+#Запуск
 
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
+В папке с проектом запустить
 
-### `yarn test`
+>make init
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+Все остальное будет сделано автоматически
 
-### `yarn build`
+Также можно без докера:
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+> npm i
+>
+> npm start
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `yarn eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,12 +1,94 @@
 import React from 'react';
-import { useTranslation } from 'react-i18next';
+import { Typography } from '@material-ui/core';
+import Link from '@material-ui/core/Link';
+import styled from 'styled-components';
+import { PageTitle } from '../../components/PageTitle';
 
 export const HomePage: React.FC = () => {
-  const { t } = useTranslation();
-
   return (
     <>
-      <div>{t('test')}</div>
+      <PageTitle>Домашняя страница</PageTitle>
+      <ChapterTitle>
+        Это демонстрационный проект, написанный с целью показать уровень
+        владения навыками разработки на react.
+      </ChapterTitle>
+      <ChapterTitle>Исходный код:</ChapterTitle>
+      <Typography>
+        Backend:&nbsp;
+        <Link
+          className="text-info"
+          href="https://github.com/horhoj/store2-back"
+          target="_blank"
+          rel="noreferrer"
+        >
+          https://github.com/horhoj/store2-back
+        </Link>
+      </Typography>
+
+      <Typography>
+        Frontend:&nbsp;
+        <Link
+          className="text-info"
+          href="https://github.com/horhoj/store2-front"
+          target="_blank"
+          rel="noreferrer"
+        >
+          https://github.com/horhoj/store2-front
+        </Link>
+      </Typography>
+
+      <ChapterTitle>Используемые технологии:</ChapterTitle>
+      <Ul>
+        <li>react</li>
+        <li>redux-toolkit</li>
+        <li>redux-saga</li>
+        <li>typescript</li>
+        <li>yup</li>
+        <li>formik</li>
+        <li>i18next</li>
+        <li>styled-components</li>
+        <li>axios</li>
+        <li>Material UI</li>
+      </Ul>
+      <ChapterTitle>Функционал проекта:</ChapterTitle>
+      <Ul>
+        <li>backend написан на PHP 8 и laravel</li>
+        <li>Общение между back и front по API REST</li>
+        <li>
+          Входящие данные от АПИ валидируются в реальном времени, с помощью
+          типов описанных посредством библиотеки yup
+        </li>
+        <li>
+          Описание сущностей выполнено на typescript, который автоматически
+          генерируется на основе описаний структур сущностей посредством yup,
+          что решает проблему двойного описания сущностей при разработки и их
+          валидации в реальном времени
+        </li>
+        <li>
+          Некоторые сложные компоненты разработаны самостоятельно (Например
+          DataGrid).
+        </li>
+        <li>
+          Многие компоненты переиспользуются, например DataGrid используется для
+          отображения списков категорий и товаров, а CategoryList отображает
+          список категорий и на странице категорий и в модальном окне для выбора
+          категории в форме редактирования товара.
+        </li>
+        <li>для управления проектом используется redux-saga.</li>
+      </Ul>
     </>
   );
 };
+
+const ChapterTitle = styled(Typography)`
+  font-size: 120%;
+  margin-top: 10px;
+  font-style: italic;
+`;
+
+const Ul = styled.ul`
+  list-style: symbols();
+  & > li {
+    margin-left: 30px;
+  }
+`;
